2bec77c6e1972cc60dc88805ac80f97bf916146f
shady <knightdawner@gmail.com>
2015-11-16 11:16:13 +0800

    Accomplish the ugly number problem

--- ugly.c
+++ ugly.c
@@ -9,15 +9,15 @@ typedef struct{
     int length; // The array length, the elements in heap
     int capacity;
 }UglyArray;
+
 // Get the user input 
 int userInput(){
     int n;
     while(true){
         printf("Which ugly number do you want?(bigger than 0)");
         scanf("%d",&n);
-        if(n>0){
-                break;
-            }
+        if(n>0)
+            break;
     }
     return n;
 }
@@ -28,15 +28,15 @@ UglyArray *initialUglyArray(int n){
             printf("Error Memory Allocation");
             exit(-1);
         }
-    ugly->length = 0;
     // Roughly calculating the length of the array
-    ugly->array = (ElemType*)malloc((3*n+1) * sizeof(ElemType));
+    ugly->array = (ElemType*)malloc((4*n+1) * sizeof(ElemType));
     if(ugly->array == NULL){
             printf("Error Memory Allocation");
             exit(-1);
         }
     // Adding the first element
     ugly->array[0].num = 1;
+    ugly->length = 1;
     ugly->capacity = 3*n+1;
     return ugly;
 }
@@ -48,6 +48,7 @@ int getRoot(UglyArray *ugly){
         }
     return ugly->array[0].num;
 }
+
 // Adjust the last element to the right place
 void upAdjust(UglyArray *ugly){
     // The index of the last element is ugly->length - 1
@@ -55,21 +56,22 @@ void upAdjust(UglyArray *ugly){
     while(track > 0){
         if(ugly->array[track].num < ugly->array[(track-1)/2].num){
             // Swap the child and parent node
-            ElemType temp = ugly->array[track];
-            ugly->array[track] = ugly->array[(track-1)/2];
-            ugly->array[(track-1)/2] = temp;
+            int temp = ugly->array[track].num;
+            ugly->array[track].num = ugly->array[(track-1)/2].num;
+            ugly->array[(track-1)/2].num = temp;
             track = (track-1)/2 ;
         }
         else{
                 break;
             }
     }
-
 }
+
 //Detemrine a node had child or not
 bool hasChildren(UglyArray *ugly, int track){
-    return track*2 + 1 > ugly->length;     
+    return track*2 + 1 < ugly->length;     
 }
+
 // Return the smallest child of the node
 int minChildren(UglyArray *ugly, int track){
     // precondition --> hasChildren is true
@@ -81,6 +83,7 @@ int minChildren(UglyArray *ugly, int track){
             return ugly->array[track*2+1].num;
         } 
 }
+
 // Return the index of the smallest child node
 int minChildIndex(UglyArray *ugly, int track){
     if(ugly->array[track*2+1].num == minChildren(ugly, track)){
@@ -88,6 +91,7 @@ int minChildIndex(UglyArray *ugly, int track){
     } 
     return track*2+2;
 }
+
 // Djust the root node to the proper plcae
 void downAdjust(UglyArray *ugly){
     int track = 0;
@@ -96,16 +100,18 @@ void downAdjust(UglyArray *ugly){
             if(ugly->array[track].num > minChildren(ugly, track)){
                 // Swap it with the smallest children
                 int index = minChildIndex(ugly, track);
-                ElemType temp = ugly->array[track];
-                ugly->array[track] = ugly->array[index];
-                ugly->array[index] = temp;
+                int temp = ugly->array[track].num;
+                ugly->array[track].num = ugly->array[index].num;
+                ugly->array[index].num = temp;
                 track = index;        
-            } 
-        else{
-                break;
             }
+            else
+                break;
         }
-    }
+        // has no children
+        else
+            break;    
+    } // End of the while
 }
 
 void addNode(UglyArray *ugly, int n){
@@ -114,16 +120,23 @@ void addNode(UglyArray *ugly, int n){
     ugly->length ++;
     upAdjust(ugly);
 }
+
 int removeRoot(UglyArray *ugly){
     int result = getRoot(ugly);
-    ugly->array[0] = ugly->array[ugly->length-1];
+    ugly->array[0].num = ugly->array[ugly->length-1].num;
     ugly->length --;
     downAdjust(ugly);
+    return result;
 }
+
 int uglyNumber(UglyArray *ugly, int n){
-    int count = 0, result = 0; while(count < n){
+    printf("The n is %d\n", n);
+    int count = 0, result = 0; 
+    while(count <= n){
             if(result != getRoot(ugly)){
                 result = removeRoot(ugly);
+                // Debugging
+                printf("%d:  %d\n", count, result);
                 addNode(ugly, result*2);
                 addNode(ugly, result*3);
                 addNode(ugly, result*5);
@@ -136,16 +149,17 @@ int uglyNumber(UglyArray *ugly, int n){
         }
         return result;
 }
+
 void main(int argc, char* argv[]){
     // Get the requirements from the user. The Nth element
     int n = userInput();
     // Initial the heap
     UglyArray *ugly = initialUglyArray(n);
     // Start to buid the heap, and maintain it
-    int result = uglyNumber(ugly, n);
-    printf("The result %d", 2);
+
     // print out the result to notify the user
+    int result = uglyNumber(ugly, n);
+    printf("The result: %d\n", result);
     free(ugly->array);
     free(ugly); 
 }
-
